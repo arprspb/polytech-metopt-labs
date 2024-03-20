64adaf2c1fe06bb27160690a36214870d31573d8
Artem Prokhorov <arprspb@gmail.com>
2024-03-20 13:58:30 +0300

    files restructured

---
+++ lab3/utils.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+class my_funciton():
+    def __init__(self, func, readable):
+        self.func = func
+        self.readable = readable
+
+    def __repr__(self):
+        return self.readable
+    
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.func(*args, **kwds)
