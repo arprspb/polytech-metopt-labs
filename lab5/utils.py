7cc1be7481149f25be58ba0b9dbc0495161c216b
Artem Prokhorov <arprspb@gmail.com>
2024-04-17 10:44:33 +0300

    Func class edit

--- lab5/utils.py
+++ lab5/utils.py
@@ -1,18 +1,15 @@
-
-
-
 class Func():
-    def __init__(f, grad):
+    def __init__(self,f, grad):
         self.f = f
         self.grad = grad
 
     def __call__(self,*args):
         return self.f(*args)
     
-    def grad():
+    def grad(self, *args):
         return self.grad(*args)
         
-f = Func(lambda *args: args[0]**2+args[1]**2, lambda *args: (2*args[0], 2*args[1]))
+f = Func(lambda x: x[0]**2+x[1]**2, lambda x: (2*x[0], 2*x[1]))
 
-print(f(5,2))
-print(f.grad(5,2))
+print(f([5,2]))
+print(f.grad([5,2]))
